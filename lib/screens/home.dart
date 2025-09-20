19212994b0c046c3bf7756740366de692e2350b4
Anant Singh <122683904+AnantKumarSingh26@users.noreply.github.com>
2025-09-20 11:05:16 +0530

    Home Screens

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,8 +7,67 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: const Placeholder(
-        child: Text('Hello  Hero'),
+      child: Scaffold(
+        backgroundColor: tdBGColor,
+        appBar: _buildAppBar(),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+          child: Column(children: [searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top :50,bottom: 20),
+                      child: Text(
+                        'All ToDos',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+// +++----------   Search Box ------------+++
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, maxWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+        ),
+      ),
+    );
+  }
+
+  // --------++++++     App Bar Method    ++++++------------
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(Icons.menu, color: tdBlack, size: 30),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(20),
+              child: Image.asset('assets/images/profile.png'),
+            ),
+          ),
+        ],
       ),
     );
   }
