32718ec3ee92a850786ad397964030d57626f540
Anant Singh <122683904+AnantKumarSingh26@users.noreply.github.com>
2025-09-23 00:02:28 +0530

    Ui Fully Completed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,9 +4,14 @@ import '../Widget/todo_item.dart';
 import '../constants/colors.dart';
 import '../model/todo.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final todList = ToDo.todoList();
 
   @override
@@ -43,54 +48,60 @@ class Home extends StatelessWidget {
                 ],
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 25, right: 20, left: 20),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(0, 0),
-                            blurRadius: 0,
-                            spreadRadius: 0,
-                          ),
-                        ],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextField(
-                        
-                        decoration: InputDecoration(
-                          hintText: 'Add a new Task',
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 25, right: 20),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('+', style: TextStyle(fontSize: 40)),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: Size(60, 60),
-                        elevation: 10,
-                        foregroundColor: Colors.white,
-                        backgroundColor: tdBlue,
-                        shape: RoundedRectangleBorder(borderRadius: 
-                        BorderRadiusGeometry.circular(10)),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            bottomNavBar()
           ],
         ),
+      ),
+    );
+  }
+  
+// +++----------    Bottom Navigation Bar -------------+++
+  Widget bottomNavBar() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              margin: EdgeInsets.only(bottom: 25, right: 20, left: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withValues(alpha: 0.7),
+                    offset: Offset(0, 0),
+                    blurRadius: 7,
+                    spreadRadius: 2,
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Add a new Task',
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 25, right: 20),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(60, 60),
+                elevation: 10,
+                foregroundColor: Colors.white,
+                backgroundColor: tdBlue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(10),
+                ),
+              ),
+              child: Text('+', style: TextStyle(fontSize: 40)),
+            ),
+          ),
+        ],
       ),
     );
   }
